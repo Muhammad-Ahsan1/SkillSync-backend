2ad56f361fcfed07c517406c986775d20567e4b6
Ali Raza <aliraza.12748g@gmail.com>
2023-12-02 00:33:10 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const compression = require("compression");
+const { PORT, NODE_ENV } = require("./config");
+const gigRoutes = require("./routes/api/gigRoutes");
+const paymentRoutes = require("./routes/api/paymentRoutes");
+const orderRoutes = require("./routes/api/orderRoutes");
+
+const isProduction = NODE_ENV === "production";
+
+if (!isProduction) {
+  require("dotenv").config();
+}
+
+app.use(compression()); // gzip compress responses
+require("./startup/logging")(); // logging to files
+require("./startup/db")(); // database connection
+require("./startup/session")(app); // Initialized session for authentication
+require("./startup/securityHeaders")(app); // Setting security headers with helmet module
+require("./startup/routes")(app); // Initializing all api routes
+
+if (isProduction) {
+  app.use(express.static("client/build"));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+} else {
+  app.use(express.static("../public"));
+  app.get("/", (req, res) => {
+    res.send("hello");
+  });
+  app.use("/api/gig", gigRoutes);
+  app.use("/api/orders", orderRoutes);
+  app.use("/api/payments", paymentRoutes);
+}
+
+app.listen(PORT, () => {
+  console.log(`> 🚀 Server is running on port ${PORT}...`);
+});
---
+++ routes/api/paymentRoutes.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const router = express.Router();
+const Payment = require("../../models/Payment");
+const GigModel = require("../../models/Gig");
+const Order = require("../../models/Order");
+const asyncMiddleware = require("../../middlewares/async");
+const stripe = require("stripe")(
+  "sk_test_51L9UqtG0EzPHWgTDVYnc5JN2mZeTJVJL4xPLUrckxRI4cLqM99NtEcfv7X9zll8k98xHajJouexwEWmcNZaDxDex00HzgsqPH4"
+);
+
+const buyGig = asyncMiddleware(async (req, res) => {
+  console.log("I am runnig🤣🤣");
+  try {
+    const { seller, buyer, gig, orderId } = req.body;
+    const orderExist = await Order.findById(orderId);
+    const gigExist = await GigModel.findById(gig);
+    if (gigExist && orderExist) {
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        mode: "payment",
+        line_items: [
+          {
+            price_data: {
+              currency: "usd",
+              product_data: {
+                name: gigExist.title,
+              },
+              unit_amount: gigExist.price * 100,
+            },
+            quantity: 1,
+          },
+        ],
+        success_url: "http://localhost:3000/pages/orders",
+        cancel_url: "http://localhost:3000/payment/cancel",
+      });
+
+      const payment = await Payment.create({
+        seller,
+        buyer,
+        amount: gigExist.price,
+        gig,
+        orderId,
+      });
+
+      if (orderExist) {
+        orderExist.status = "PAID";
+        const updatedOrder = await orderExist.save();
+      }
+
+      res.json({ url: session.url });
+      // console.log(session);
+      if (payment) {
+        res.status(201);
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+const getAllPayments = asyncMiddleware(async (req, res) => {
+  const allPayments = await Payment.find({}).populate("gig seller buyer");
+  res.json(allPayments);
+});
+
+router.post("/checkout", buyGig);
+router.get("/", getAllPayments);
+
+module.exports = router;
