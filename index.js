a404d921673a4a70ddb3083ab2b1c575d1970ded
Ali Raza <aliraza.12748g@gmail.com>
2023-12-03 02:51:33 +0500

    add backedn file

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ const { PORT, NODE_ENV } = require("./config");
 const gigRoutes = require("./routes/api/gigRoutes");
 const paymentRoutes = require("./routes/api/paymentRoutes");
 const orderRoutes = require("./routes/api/orderRoutes");
+const cors =require('cors')
+const isProduction = NODE_ENV === "production";
 
-// const isProduction = NODE_ENV === "production";
-
-// if (!isProduction) {
+if (!isProduction) {
 require("dotenv").config();
-// }
+}
 app.use(cors(
   {
     origin: "*",
-    methods: ["POST", "GET", "PUT", "DELETE", "OPTIONS"],
+    // methods: ["POST", "GET", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
 
   }
@@ -27,22 +27,22 @@ require("./startup/session")(app); // Initialized session for authentication
 require("./startup/securityHeaders")(app); // Setting security headers with helmet module
 require("./startup/routes")(app); // Initializing all api routes
 
-// if (isProduction) {
-//   app.use(express.static("client/build"));
+if (isProduction) {
+  app.use(express.static("client/build"));
 
-//   app.get("*", (_, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// } else {
+  app.get("*", (_, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+} else {
 
-// app.use(express.static("../public"));
+app.use(express.static("../public"));
 app.get("/hello", (req, res) => {
   res.send("hello");
 });
 app.use("/api/gig", gigRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/payments", paymentRoutes);
-// }
+}
 
 app.listen(PORT, () => {
   console.log(`> 🚀 Server is running on port ${PORT}...`);
